57eec5633d7bebe44caa469fe71e44d5a10b7dbc
François Pottier <francois.pottier@inria.fr>
2024-12-30 15:35:42 +0100

    GYOR.ml: remove [uncolored], which is unused. Also, reformat exactly as in the paper.

--- lib/color/GYOR.ml
+++ lib/color/GYOR.ml
@@ -1,19 +1,14 @@
-(* Defining some hues. *)
+(* A group of distinct types encode the presence or absence of a hue. *)
 type somegreen  = SOME_GREEN
 type nogreen    = NO_GREEN
-
 type someyellow = SOME_YELLOW
 type noyellow   = NO_YELLOW
-
 type someorange = SOME_ORANGE
 type noorange   = NO_ORANGE
-
 type somered    = SOME_RED
 type nored      = NO_RED
-
-(* Defining colors. *)
-type green  = somegreen * noyellow   * noorange   * nored
-type yellow = nogreen   * someyellow * noorange   * nored
-type orange = nogreen   * noyellow   * someorange * nored
-type red    = nogreen   * noyellow   * noorange   * somered
-type uncolored = nogreen * noyellow * noorange * nored
+(* A color is a quadruple of four hue bits. *)
+type green      =  somegreen *   noyellow *   noorange *   nored
+type yellow     =    nogreen * someyellow *   noorange *   nored
+type orange     =    nogreen *   noyellow * someorange *   nored
+type red        =    nogreen *   noyellow *   noorange * somered
