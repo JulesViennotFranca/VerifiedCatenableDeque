48dbba8edf44b7183382e954fe6d8587680d7e17
Jules Viennot Franca <jules.viennot@gmail.com>
2025-01-20 18:32:13 +0100

    Cadeques buffers update

--- theory/cadeque/buffer.v
+++ theory/cadeque/buffer.v
@@ -187,12 +187,6 @@ Qed.
 (* The last lemma is added to the hint database. *)
 #[export] Hint Rewrite empty_buffer : rbuffer.
 
-(* Returns the two elements of a buffer containing two elements. *)
-Equations two {A} (b : t A 2) : { '(a1, a2) : A * A | seq b = [a1] ++ [a2] } :=
-two b with pop b => {
-  | ? (a1, b') with pop b' => {
-    | ? (a2, _) := ? (a1, a2) } }.
-
 (* Returns a buffer containing one element. *)
 Equations single {A} (a1 : A) : { b : t A 1 | seq b = [a1] } :=
 single a1 with deque.empty => {
@@ -260,15 +254,6 @@ Equations inject8 {A q} (b : t A q) (a8 a7 a6 a5 a4 a3 a2 a1 : A) :
 inject8 b a8 a7 a6 a5 a4 a3 a2 a1 with inject6 b a8 a7 a6 a5 a4 a3 => {
   | ? b' with inject2 b' a2 a1 => { | ? b'' := ? b'' } }.
 
-(* Pops eight elements off a buffer containing at least eight elements. *)
-Equations pop8 {A q} (b : t A (8 + q)) :
-  { '(a1, a2, a3, a4, a5, a6, a7, a8, b') :
-    A * A * A * A * A * A * A * A * t A q | seq b =
-    [a1] ++ [a2] ++ [a3] ++ [a4] ++ [a5] ++ [a6] ++ [a7] ++ [a8] ++ seq b' } :=
-pop8 b with pop b => { | ? (a1, b') with pop5 b' => {
-  | ? (a2, a3, a4, a5, a6, b'') with pop2 b'' => {
-    | ? (a7, a8, b''') := ? (a1, a2, a3, a4, a5, a6, a7, a8, b''') } } }.
-
 (* Provided the equality of two natural numbers, translates a buffer of size
    the first into a buffer of size the second. *)
 Equations translate {A q1 q2} (b : t A q1) (eq : q1 = q2) :
@@ -323,10 +308,10 @@ push_5vector a1 a2 a3 a4 a5 vec b with push_vector vec b => {
 
 (* Injects five elements the a vector on a buffer. *)
 Equations inject_5vector {A n q}
-  (b : t A q) (a5 a4 a3 a2 a1 : A) (vec : vector A n) :
+  (b : t A q) (z5 z4 z3 z2 z1 : A) (vec : vector A n) :
   { b' : t A (5 + q + vector_size vec) | seq b' =
-    seq b ++ [a5] ++ [a4] ++ [a3] ++ [a2] ++ [a1] ++ vector_seq vec } :=
-inject_5vector b a5 a4 a3 a2 a1 vec with inject5 b a5 a4 a3 a2 a1 => {
+    seq b ++ [z5] ++ [z4] ++ [z3] ++ [z2] ++ [z1] ++ vector_seq vec } :=
+inject_5vector b z5 z4 z3 z2 z1 vec with inject5 b z5 z4 z3 z2 z1 => {
   | ? b' with inject_vector b' vec => { | ? b'' := ? b'' } }.
 
 (* Takes a buffer and returns an option indicating wheter the buffer is empty
@@ -349,60 +334,6 @@ has3 (q := 0) b := ? inl V0;
 has3 (q := 1) b with pop b => { | ? (a1, _) := ? inl (V1 a1) };
 has3 (q := 2) b with pop2 b => { | ? (a1, a2, _) := ? inl (V2 a1 a2) };
 has3 (q := _) b := ? inr b.
-
-(* Takes a buffer of at least four elements and returns a sum indicating
-   whether the buffer contains exactly four or more than five elements. *)
-Equations has5 {A q} (b : t A (4 + q)) :
-  { s : sum (A * A * A * A) (pt A 5 (4 + q)) |
-    seq b = match s with
-            | inl (a1, a2, a3, a4) => [a1] ++ [a2] ++ [a3] ++ [a4]
-            | inr b' => seq b'
-            end } :=
-has5 (q := 0) b with pop2 b => {
-  | ? (a1, a2, b') with two b' => {
-    | ? (a3, a4) := ? inl (a1, a2, a3, a4) } };
-has5 (q := _) b := ? inr b.
-
-(* Takes a buffer of at least one element and returns a sum indicating whether
-   the buffer contains less or strictly more than six elements. *)
-Equations has7 {A q} (b : t A (S q)) :
-  { s : sum (vector A 6) (pt A 7 (S q)) |
-    seq b = match s with
-            | inl vec => vector_seq vec
-            | inr b' => seq b'
-            end } :=
-has7 (q := 0) b with pop b => { | ? (a1, _) := ? inl (V1 a1) };
-has7 (q := 1) b with pop2 b => { | ? (a1, a2, _) := ? inl (V2 a1 a2) };
-has7 (q := 2) b with pop2 b => {
-  | ? (a1, a2, b') with pop b' => { | ? (a3, _) := ? inl (V3 a1 a2 a3) } };
-has7 (q := 3) b with pop2 b => {
-  | ? (a1, a2, b') with pop2 b' => {
-    | ? (a3, a4, _) := ? inl (V4 a1 a2 a3 a4) } };
-has7 (q := 4) b with pop5 b => {
-  | ? (a1, a2, a3, a4, a5, _) := ? inl (V5 a1 a2 a3 a4 a5) };
-has7 (q := 5) b with pop5 b => {
-  | ? (a1, a2, a3, a4, a5, b') with pop b' => {
-    | ? (a6, _) := ? inl (V6 a1 a2 a3 a4 a5 a6) } };
-has7 (q := _) b := ? inr b.
-
-(* Takes a buffer of at least five elements and returns a sum indicating
-   whether the buffer contains less or strictly more than seven elements. *)
-Equations has8 {A q} (b : t A (5 + q)) :
-  { s : sum (A * A * A * A * A * vector A 2) (pt A 8 (5 + q)) |
-    seq b = match s with
-            | inl (a1, a2, a3, a4, a5, vec) =>
-              [a1] ++ [a2] ++ [a3] ++ [a4] ++ [a5] ++ vector_seq vec
-            | inr b' => seq b'
-            end } :=
-has8 (q := 0) b with pop5 b => {
-  | ? (a1, a2, a3, a4, a5, _) := ? inl (a1, a2, a3, a4, a5, V0) };
-has8 (q := 1) b with pop5 b => {
-  | ? (a1, a2, a3, a4, a5, b') with pop b' => {
-    | ? (a6, _) := ? inl (a1, a2, a3, a4, a5, V1 a6) } };
-has8 (q := 2) b with pop5 b => {
-  | ? (a1, a2, a3, a4, a5, b') with two b' => {
-    | ? (a6, a7) := ? inl (a1, a2, a3, a4, a5, V2 a6 a7) } };
-has8 (q := _) b := ? inr b.
 
 (* Takes a buffer of at least three elements and returns its first three
    elements along with a sum indicating whether the remaining buffer contains
@@ -432,6 +363,86 @@ has3s b with eject2 b => { | ? (b', a2, a1) with eject b' => {
   | ? (b'', a3) with has3 b'' => {
     | ? s := ? (s, (a3, a2, a1)) } } }.
 
+(* Takes a buffer of at least four elements and returns a sum indicating
+   whether the buffer contains exactly four or more than five elements. *)
+Equations has5 {A q} (b : t A (4 + q)) :
+  { s : sum (A * A * A * A) (pt A 5 (4 + q)) |
+    seq b = match s with
+            | inl (a1, a2, a3, a4) => [a1] ++ [a2] ++ [a3] ++ [a4]
+            | inr b' => seq b'
+            end } :=
+has5 (q := 0) b with pop2 b => {
+  | ? (a1, a2, b') with pop2 b' => {
+    | ? (a3, a4, _) := ? inl (a1, a2, a3, a4) } };
+has5 (q := _) b := ? inr b.
+
+(* Takes a buffer of at least one element and returns a sum indicating whether
+    the buffer contains strictly less or more than seven elements. If the
+    buffer contains more than seven elements, its last two elements are
+    extracted. *)
+Equations has7s {A q} (b : t A (S q)) :
+  { s : sum (vector A 6) (pt A 5 (Nat.pred q) * A * A) |
+    seq b = match s with
+            | inl vec => vector_seq vec
+            | inr (b', z2, z1) => seq b' ++ [z2] ++ [z1]
+            end } :=
+has7s (q := 0) b with eject b => { | ? (_, z1) := ? inl (V1 z1) };
+has7s (q := 1) b with eject2 b => { | ? (_, z2, z1) := ? inl (V2 z2 z1) };
+has7s (q := 2) b with eject2 b => {
+  | ? (b', z2, z1) with eject b' => { | ? (_, z3) := ? inl (V3 z3 z2 z1) } };
+has7s (q := 3) b with eject2 b => {
+  | ? (b', z2, z1) with eject2 b' => {
+    | ? (_, z4, z3) := ? inl (V4 z4 z3 z2 z1) } };
+has7s (q := 4) b with pop5 b => {
+  | ? (z5, z4, z3, z2, z1, _) := ? inl (V5 z5 z4 z3 z2 z1) };
+has7s (q := 5) b with pop5 b => {
+  | ? (z6, z5, z4, z3, z2, b') with eject b' => {
+    | ? (_, z1) := ? inl (V6 z6 z5 z4 z3 z2 z1) } };
+has7s (q := _) b with eject2 b => { | ? r := ? inr r }.
+
+(* Takes a buffer of at least one element and returns a sum indicating whether
+    the buffer contains strictly less or more than seven elements. If the
+    buffer contains more than seven elements, its first two elements are
+    extracted. *)
+Equations has7p {A q} (b : t A (S q)) :
+  { s : sum (vector A 6) (A * A * pt A 5 (Nat.pred q)) |
+    seq b = match s with
+            | inl vec => vector_seq vec
+            | inr (a1, a2, b') => [a1] ++ [a2] ++ seq b'
+            end } :=
+has7p (q := 0) b with pop b => { | ? (a1, _) := ? inl (V1 a1) };
+has7p (q := 1) b with pop2 b => { | ? (a1, a2, _) := ? inl (V2 a1 a2) };
+has7p (q := 2) b with pop2 b => {
+  | ? (a1, a2, b') with pop b' => { | ? (a3, _) := ? inl (V3 a1 a2 a3) } };
+has7p (q := 3) b with pop2 b => {
+  | ? (a1, a2, b') with pop2 b' => {
+    | ? (a3, a4, _) := ? inl (V4 a1 a2 a3 a4) } };
+has7p (q := 4) b with pop5 b => {
+  | ? (a1, a2, a3, a4, a5, _) := ? inl (V5 a1 a2 a3 a4 a5) };
+has7p (q := 5) b with pop5 b => {
+  | ? (a1, a2, a3, a4, a5, b') with pop b' => {
+    | ? (a6, _) := ? inl (V6 a1 a2 a3 a4 a5 a6) } };
+has7p (q := _) b with pop2 b => { | ? r := ? inr r }.
+
+(* Takes a buffer of at least five elements and returns a sum indicating
+   whether the buffer contains less or strictly more than seven elements. *)
+Equations has8 {A q} (b : t A (5 + q)) :
+  { s : sum (A * A * A * A * A * vector A 2) (pt A 8 (5 + q)) |
+    seq b = match s with
+            | inl (a1, a2, a3, a4, a5, vec) =>
+              [a1] ++ [a2] ++ [a3] ++ [a4] ++ [a5] ++ vector_seq vec
+            | inr b' => seq b'
+            end } :=
+has8 (q := 0) b with pop5 b => {
+  | ? (a1, a2, a3, a4, a5, _) := ? inl (a1, a2, a3, a4, a5, V0) };
+has8 (q := 1) b with pop5 b => {
+  | ? (a1, a2, a3, a4, a5, b') with pop b' => {
+    | ? (a6, _) := ? inl (a1, a2, a3, a4, a5, V1 a6) } };
+has8 (q := 2) b with pop5 b => {
+  | ? (a1, a2, a3, a4, a5, b') with pop2 b' => {
+    | ? (a6, a7, _) := ? inl (a1, a2, a3, a4, a5, V2 a6 a7) } };
+has8 (q := _) b := ? inr b.
+
 (* Takes a buffer of at least eight elements and returns a sum indicating
    whether the buffer contains less or strictly more than 10 elements. *)
 Equations has3p8 {A q} (b : t A (8 + q)) :
@@ -443,15 +454,9 @@ Equations has3p8 {A q} (b : t A (8 + q)) :
               vector_seq vec
             | inr (b3, b8) => seq b3 ++ seq b8
             end } :=
-has3p8 (q := 0) b with pop8 b => {
-  | ? (a1, a2, a3, a4, a5, a6, a7, a8, _) :=
-    ? inl (a1, a2, a3, a4, a5, a6, a7, a8, V0) };
-has3p8 (q := 1) b with pop8 b => {
-  | ? (a1, a2, a3, a4, a5, a6, a7, a8, b') with pop b' => {
-    | ? (a9, _) := ? inl (a1, a2, a3, a4, a5, a6, a7, a8, V1 a9) } };
-has3p8 (q := 2) b with pop8 b => {
-  | ? (a1, a2, a3, a4, a5, a6, a7, a8, b') with pop2 b' => {
-    | ? (a9, a10, _) := ? inl (a1, a2, a3, a4, a5, a6, a7, a8, V2 a9 a10) } };
-has3p8 (q := _) b with pop2 b => { | ? (a1, a2, b') with pop b' => {
-    | ? (a3, b8) with single a3 => { | ? b1 with push2 a1 a2 b1 => {
-      | ? b3 := ? inr (b3, b8) } } } }.
+has3p8 b with pop2 b => { | ? (a1, a2, b') with pop b' => {
+  | ? (a3, b'') with has8 b'' => {
+    | ? inl (a4, a5, a6, a7, a8, v) :=
+      ? inl (a1, a2, a3, a4, a5, a6, a7, a8, v);
+    | ? inr br with pair a2 a3 => { | ? bl with push a1 bl => {
+        | ? bl' := ? inr (bl', br) } } } } }.
