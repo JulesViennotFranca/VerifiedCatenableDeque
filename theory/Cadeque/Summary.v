a27af1d14f450a68c0fda192296152c4a81ae7eb
François Pottier <francois.pottier@inria.fr>
2025-11-21 13:36:12 +0100

    Define the signatures of catenable deques in Signatures.v, and check that they are satisfied in Summary.v.

---
+++ theory/Cadeque/Summary.v
@@ -0,0 +1,96 @@
+From Coq Require Import List.
+Import ListNotations.
+From Deques.Cadeque Require Import Types Models Core Operations.
+From Deques Require Import Signatures.
+
+(* We check that the types and operations defined in this directory
+   satisfy (the two variants of) the signature of catenable deques,
+   as defined (one level up) in Signatures.v. *)
+
+(* The intrinsic signature. *)
+
+Module I : CADEQUE_INTRINSIC.
+  Definition cadeque := Types.cadeque.
+  Definition model   := @Models.cadeque_seq.
+  Definition empty   := @Operations.D.empty.
+  Definition push    := @Operations.D.push.
+  Definition inject  := @Operations.D.inject.
+  Definition pop     := @Operations.D.pop.
+  Definition eject   := @Operations.D.eject.
+  Definition concat  := @Operations.D.concat.
+End I.
+
+(* The extrinsic signature is satisfied by splitting up each operation
+   in two components: the operation itself and its proof of correctness.
+   The two pair projections are [proj1_sig] and [proj2_sig]. *)
+
+Module E : CADEQUE_EXTRINSIC.
+
+  Definition cadeque   :=
+    Types.cadeque.
+
+  Definition model {A} :=
+    (@Models.cadeque_seq A).
+
+  Definition empty {A} :=
+    proj1_sig (@Operations.D.empty A).
+  Definition empty_correct {A} :=
+    proj2_sig (@Operations.D.empty A).
+
+  Definition push {A} (x : A) (d : cadeque A) :=
+    proj1_sig (Operations.D.push x d).
+  Definition push_correct {A : Type} (x : A) (d : cadeque A) :=
+    proj2_sig (Operations.D.push x d).
+
+  Definition inject {A} (d : cadeque A) (x : A) :=
+    proj1_sig (Operations.D.inject d x).
+  Definition inject_correct {A} (d : cadeque A) (x : A) :=
+    proj2_sig (Operations.D.inject d x).
+
+  Definition pop {A} (d : cadeque A) :=
+    proj1_sig (Operations.D.pop d).
+  Definition pop_correct {A} (d : cadeque A) :=
+    proj2_sig (Operations.D.pop d).
+
+  Definition eject {A} (d : cadeque A) :=
+    proj1_sig (Operations.D.eject d).
+  Definition eject_correct {A} (d : cadeque A) :=
+    proj2_sig (Operations.D.eject d).
+
+  Definition concat {A} (d1 d2 : cadeque A) :=
+    proj1_sig (Operations.D.concat d1 d2).
+  Definition concat_correct {A : Type} (d1 d2 : cadeque A) :=
+    proj2_sig (Operations.D.concat d1 d2).
+
+End E.
+
+(* Finally, check that no axioms were used, no proofs were admitted, etc. *)
+
+(* I don't know how to apply Print Assumptions to a module,
+   so I apply it to every identifier in the module I and E. *)
+
+(* This should print 22 times [Closed under the global context]. *)
+
+Print Assumptions I.cadeque.
+Print Assumptions I.model.
+Print Assumptions I.empty.
+Print Assumptions I.push.
+Print Assumptions I.pop.
+Print Assumptions I.inject.
+Print Assumptions I.eject.
+Print Assumptions I.concat.
+
+Print Assumptions E.cadeque.
+Print Assumptions E.model.
+Print Assumptions E.empty.
+Print Assumptions E.empty_correct.
+Print Assumptions E.push.
+Print Assumptions E.push_correct.
+Print Assumptions E.pop.
+Print Assumptions E.pop_correct.
+Print Assumptions E.inject.
+Print Assumptions E.inject_correct.
+Print Assumptions E.eject.
+Print Assumptions E.eject_correct.
+Print Assumptions E.concat.
+Print Assumptions E.concat_correct.
---
+++ theory/Signatures.v
@@ -0,0 +1,137 @@
+From Coq Require Import List.
+Import ListNotations.
+
+(* The following module types, or signatures, sum up the types and operations
+   that must be offered by an implementation of catenable deques, as well as
+   the correctness properties that these operations must satisfy.
+
+   We propose two signatures. The signature [CADEQUE_INTRINSIC] is expressed
+   in intrinsic style: the correctness statement of each operation is built
+   into its type via a subset type, also known as a Σ type, or [sig] in Rocq.
+   The signature [CADEQUE_EXTRINSIC] is in extrinsic style: each operation has
+   a simple type, and each correctness statement forms a separate lemma. *)
+
+Module Type CADEQUE_INTRINSIC.
+
+  (* The type [cadeque A] describes a catenable deque, or cadeque,
+     whose elements have type [A]. *)
+  Parameter cadeque : Type -> Type.
+
+  (* The model of a cadeque is the sequence of its elements.
+     To represent mathematical sequences, we use Rocq's lists.
+     Thus, if [d] is a cadeque then [model d] is a list.
+
+     This operation is not intended to be used at runtime
+     (although it could). It is needed in order to express
+     the correctness statement of each operation. *)
+  Parameter model :
+    forall {A}, cadeque A -> list A.
+
+  (* [empty] is the empty cadeque. *)
+  Parameter empty :
+    forall {A},
+    { d : cadeque A |
+      model d = []
+    }.
+
+  (* [push x d] inserts [x] at the left end of the cadeque [d]. *)
+  Parameter push :
+    forall {A} (x : A) (d : cadeque A),
+    { d' : cadeque A |
+      model d' = [x] ++ model d
+    }.
+
+  (* [inject x d] inserts [x] at the right end of the cadeque [d]. *)
+  Parameter inject :
+    forall {A} (d : cadeque A) (x : A),
+    { d' : cadeque A |
+      model d' = model d ++ [x]
+    }.
+
+  (* [pop d] attempts to extract an element at the left end of the cadeque
+     [d]. It returns either nothing or a pair of an element and a cadeque. *)
+  Parameter pop :
+    forall {A} (d : cadeque A),
+    { o : option (A * cadeque A) |
+      model d = match o with
+                      | None => []
+                      | Some (x, d') => [x] ++ model d'
+                      end
+    }.
+
+  (* [eject d] attempts to extract an element at the right end of the cadeque
+     [d]. It returns either nothing or a pair of a cadeque and an element. *)
+  Parameter eject :
+    forall {A} (d : cadeque A),
+    { o : option (cadeque A * A) |
+      model d = match o with
+                      | None => []
+                      | Some (d', x) => model d' ++ [x]
+                      end
+    }.
+
+  (* [concat] concatenates two cadeques. *)
+  Parameter concat :
+    forall {A : Type} (d1 d2 : cadeque A),
+    { d : cadeque A |
+      model d = model d1 ++ model d2
+    }.
+
+End CADEQUE_INTRINSIC.
+
+Module Type CADEQUE_EXTRINSIC.
+
+  Parameter cadeque : Type -> Type.
+
+  Parameter model :
+    forall {A}, cadeque A -> list A.
+
+  Parameter empty :
+    forall {A}, cadeque A.
+
+  Parameter empty_correct :
+    forall {A},
+    model (empty : cadeque A) = [].
+
+  Parameter push :
+    forall {A}, A -> cadeque A -> cadeque A.
+
+  Parameter push_correct :
+    forall {A} (x : A) (d : cadeque A),
+    model (push x d) = [x] ++ model d.
+
+  Parameter inject :
+    forall {A}, cadeque A -> A -> cadeque A.
+
+  Parameter inject_correct :
+    forall {A} (d : cadeque A) (x : A),
+    model (inject d x) = model d ++ [x].
+
+  Parameter pop :
+    forall {A}, cadeque A -> option (A * cadeque A).
+
+  Parameter pop_correct :
+    forall {A} (d : cadeque A),
+    model d = match pop d with
+              | None => []
+              | Some (x, d') => [x] ++ model d'
+              end.
+
+  Parameter eject :
+    forall {A}, cadeque A -> option (cadeque A * A).
+
+  Parameter eject_correct :
+    forall {A} (d : cadeque A),
+    model d = match eject d with
+              | None => []
+              | Some (d', x) => model d' ++ [x]
+              end.
+
+  Parameter concat :
+    forall {A : Type}, cadeque A -> cadeque A -> cadeque A.
+
+  Parameter concat_correct :
+    forall {A : Type} (d1 d2 : cadeque A),
+    model (concat d1 d2) = model d1 ++ model d2.
+
+End CADEQUE_EXTRINSIC.
