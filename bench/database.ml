a6eec3119c51c76b1a76dc0300ccb363185b62be
François Pottier <francois.pottier@inria.fr>
2025-12-22 13:47:12 +0100

    More functions in the module Vector.

--- bench/database.ml
+++ bench/database.ml
@@ -51,38 +51,56 @@ module Vector = struct
   type 'a t =
     { array : 'a array; mutable length : int }
 
-  let create ~size ~dummy =
-    { array = Array.make size dummy ; length = 0 }
+  let[@inline] create ~size ~dummy =
+    { array = Array.make size dummy; length = 0 }
 
-  let get v i =
+  let[@inline] length v =
+    v.length
+
+  let[@inline] is_empty v =
+    v.length = 0
+
+  let[@inline] is_full v =
+    Array.length v.array = v.length
+
+  let[@inline] clear v =
+    v.length <- 0
+
+  let[@inline] get v i =
     assert (0 <= i && i < v.length);
     v.array.(i)
 
-  let push v x =
+  let[@inline] set v i x =
+    assert (0 <= i && i < v.length);
+    v.array.(i) <- x
+
+  let[@inline] push v x =
     assert (v.length < Array.length v.array);
     v.array.(v.length) <- x;
     v.length <- v.length + 1
 
-  let is_full v =
-    Array.length v.array = v.length
+  let[@inline] pop v =
+    assert (0 < v.length);
+    v.length <- v.length - 1;
+    v.array.(v.length)
 
-  let iter f v =
+  let[@inline] iter f v =
     for i = 0 to v.length - 1 do
       f v.array.(i)
     done
 
-  let iter2 f v1 v2 =
+  let[@inline] iter2 f v1 v2 =
     for i = 0 to v1.length - 1 do
       for j = 0 to v2.length - 1 do
         f v1.array.(i) v2.array.(j)
       done;
     done
 
-  let to_list v =
-    List.init v.length (Array.get v.array)
+  let[@inline] to_list v =
+    List.init v.length (get v)
 
-  let sample v =
-    v.array.(Random.int v.length)
+  let[@inline] sample v =
+    get v (Random.int v.length)
 
 end
 
