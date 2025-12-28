5fa2852495edcd54cd778dd82a6651abd9b962d7
François Pottier <francois.pottier@inria.fr>
2025-12-28 12:50:31 +0100

    BinCounting/Core.v: comments.

--- theory/BinCounting/Core.v
+++ theory/BinCounting/Core.v
@@ -64,11 +64,11 @@ Unset Equations Transparent.
 (* Notation for dependent types hiding the property on [x]. *)
 Notation "? x" := (@exist _ _ x _) (at level 100).
 
+(* The following attempt at defining [green_of_red] fails, because it is
+   an ordinary Rocq [Definition], and it is incomplete: not every case is
+   covered. The cases that we have omitted are dead, but Rocq cannot see
+   it. The error message is:
 
-(* Wrong definition : it doesn't use [Equations] to rule out dead branches in
-                      the pattern matching.
-
-   Error :
    Non exhaustive pattern-matching: no clause found for pattern Chain G _ _ *)
 
 Fail Definition green_of_red (c : chain red) : chain green :=
@@ -81,9 +81,28 @@ Fail Definition green_of_red (c : chain red) : chain green :=
       Chain G (GDigit Hole) (Chain R (RDigit body) c)
   end.
 
-(* Definition without subset types. *)
-Module NoSubsetType.
-  (* Makes a red chain green. *)
+(* To avoid this problem, we use Equations. Equations assumes that a missing
+   branch must be dead. If it can prove automatically that a branch is dead
+   then it does not warn about the absence of this branch. If it is unable to
+   prove that a branch is dead then it generates a proof obligation which the
+   user must explicitly address. The tactic that is implicitly used by
+   Equations can be chosen via the command [Obligation Tactic]. For more
+   information, see the Equations tutorial:
+   https://rocq-prover.github.io/platform-docs/equations/tutorial_obligations.html *)
+
+(* Our second definition of [green_of_red] uses Equations. This time, Equations
+   is able to automatically prove that the missing branches are dead, so it does
+   not complain about them, and the definition is accepted. *)
+
+(* In this definition, we do not annotate [green_of_red] with a postcondition;
+   that is, we do not state, a priori, that the chain [green_of_red c]
+   represents the same natural number as the chain [c]. Instead, we prove
+   this fact a posteriori, in the lemma [green_of_red_correct]. The proof
+   is verbose because it must inspect the definition of [green_of_red]. *)
+
+Module NoPostcondition.
+
+  (* [green_of_red] turns a red chain into a green chain. *)
   Equations green_of_red : chain red -> chain green :=
   green_of_red (Chain R (RDigit Hole) Empty) :=
     Chain G (GDigit (YDigit Hole)) Empty;
@@ -92,10 +111,10 @@ Module NoSubsetType.
   green_of_red (Chain R (RDigit (YDigit body)) c) :=
     Chain G (GDigit Hole) (Chain R (RDigit body) c).
 
-  (* Proves that a red chain [c] and [green_of_red c] represent the same
-     natural number. *)
+  (* A red chain [c] and the chain [green_of_red c] represent the
+     same natural number. *)
   Lemma green_of_red_correct (c : chain red) :
-      chain_nat (green_of_red c) = chain_nat c.
+    chain_nat (green_of_red c) = chain_nat c.
   Proof.
     dependent elimination c.
     dependent elimination r.
@@ -107,18 +126,23 @@ Module NoSubsetType.
       + dependent elimination r.
         dependent elimination p.
         simp green_of_red. simpl.
-        remember (packet_nat p (chain_nat c)) as numb.
         lia.
     - simp green_of_red.
       simpl.
-      remember (packet_nat p0 (chain_nat c)) as numb.
       lia.
   Qed.
-End NoSubsetType.
 
-(* Makes a red chain green. *)
+End NoPostcondition.
+
+(* Our third and last definition of [green_of_red] also uses Equations. This
+   time, we annotate [green_of_red] with the postcondition
+   [chain_nat c' = chain_nat c]: that is, we claim, a priori, that the chain
+   [green_of_red c] represents the same natural number as the chain [c].
+   This gives rise to a number of proof obligations, all of which are
+   automatically proved by Equations. *)
+
 Equations green_of_red (c : chain red) :
-  { c' : chain green | chain_nat c' = chain_nat c} :=
+  { c' : chain green | chain_nat c' = chain_nat c } :=
 green_of_red (Chain R (RDigit Hole) Empty) :=
   ? Chain G (GDigit (YDigit Hole)) Empty;
 green_of_red (Chain R (RDigit Hole) (Chain G (GDigit body) c)) :=
@@ -126,7 +150,7 @@ green_of_red (Chain R (RDigit Hole) (Chain G (GDigit body) c)) :=
 green_of_red (Chain R (RDigit (YDigit body)) c) :=
   ? Chain G (GDigit Hole) (Chain R (RDigit body) c).
 
-(* Makes a green or red chain green. *)
+(* [ensure_green] turns a green or red chain into a green chain. *)
 Equations ensure_green {g r} (c : chain (Mix g NoYellow r)) :
   { c' : chain green | chain_nat c' = chain_nat c } :=
 ensure_green Empty := ? Empty;
@@ -138,10 +162,27 @@ ensure_green (Chain R pkt c) with green_of_red (Chain R pkt c) => {
 (* |                               Operation                                | *)
 (* +------------------------------------------------------------------------+ *)
 
-(* Adds one to a number. *)
+(* [succ n] returns the successor of the number [n]. *)
+
+(* Again, we define [succ] using Equations, and we state its correctness
+   property [number_nat n' = S (number_nat n)] as part of its type.
+
+   Somewhat miraculously perhaps, all of the type-checking obligations and proof
+   obligations are automatically proved, so we have nothing left to prove by
+   hand. The system automatically checks that all cases are covered and that a
+   correct result is returned in each case.
+
+   One of the reasons why full automation is achieved here is that our
+   hypotheses and goals are simple; they are equations between colors and
+   equations between natural numbers. Another reason is that at every use of
+   [ensure_green], the postcondition of [ensure_green] is automatically
+   introduced into the context by Equations, and can then be exploited in the
+   proof. *)
+
 Equations succ (n : number) :
   { n' : number | number_nat n' = S (number_nat n) } :=
-succ (T Empty) := ? T (Chain Y (YDigit Hole) Empty);
+succ (T Empty) :=
+  ? T (Chain Y (YDigit Hole) Empty);
 succ (T (Chain G (GDigit body) c)) with ensure_green c => {
   | ? c' := ? T (Chain Y (YDigit body) c') };
 succ (T (Chain Y (YDigit body) c))
